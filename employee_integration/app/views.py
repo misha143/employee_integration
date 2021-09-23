46a7d8de974bdd197f4df432fed1caee3e8c808e
Michael <doctorbf3@gmail.com>
2021-09-23 12:38:25 +0500

    new v

--- employee_integration/app/views.py
+++ employee_integration/app/views.py
@@ -4,9 +4,11 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth import get_user_model
 from datetime import datetime
 import csv
+from collections import Counter
 from django.http import HttpResponse
 
 import telegram
+
 bot = telegram.Bot(token='1989836081:AAEiKOW4TH7mP1WDANebvLfyEpnjxwhawQk')
 
 from .models import Question, Answer, Result
@@ -28,10 +30,12 @@ def quiz_view(request, pk_quiz):
     # проверка прошёл квест или нет
     is_done = 0
     is_correct = 0
-
+    user_answer = ''
     if Result.objects.filter(question__pk=pk_quiz, user__username=request.user.username).exists():
         is_done = 1
-        if Result.objects.get(question__pk=pk_quiz, user__username=request.user.username).is_correct:
+        temp = Result.objects.get(question__pk=pk_quiz, user__username=request.user.username)
+        user_answer = temp.answer
+        if temp.is_correct:
             is_correct = 1
 
     # получаем квиз и ответы
@@ -39,20 +43,31 @@ def quiz_view(request, pk_quiz):
     ans_list = Answer.objects.filter(question=quiz).all()
 
     # сколько прошли, какой процент
-    count_of_people_done = len(Result.objects.filter(question__pk=pk_quiz).all())
+    data_results = Result.objects.filter(question__pk=pk_quiz).all()
+    count_of_people_done = len(data_results)
     count_of_people_done_correct = len(Result.objects.filter(question__pk=pk_quiz, is_correct=True).all())
     if count_of_people_done == 0:
         percent_of_done_correct = 0
     else:
         percent_of_done_correct = round(count_of_people_done_correct / count_of_people_done * 100)
 
+    # самый активный отдел
+    groups_list = []
+    for d in data_results:
+        for g in d.user.groups.all():
+            groups_list.append(str(g))
+    dictt = dict(Counter(groups_list))
+    common_groups = dict(sorted(dictt.items(), key=lambda item: item[1], reverse=True))
+
     return render(request, 'quiz.html', {
         "is_done": is_done,
         "is_correct": is_correct,
+        "user_answer": user_answer,
         "quiz": quiz,
         "ans_list": ans_list,
         "count_of_people_done": count_of_people_done,
-        "percent_of_done_correct": percent_of_done_correct
+        "percent_of_done_correct": percent_of_done_correct,
+        "common_groups": common_groups.items(),
     })
 
 
@@ -64,7 +79,8 @@ def quiz_pass(request, pk_quiz, pk_ans):
         question = get_object_or_404(Question, pk=pk_quiz)
         ans = get_object_or_404(Answer, pk=pk_ans)
         Result.objects.create(question=question, user=user, is_correct=ans.correct, answer=ans)
-        bot.sendMessage(chat_id=386589423, text=f"{user.first_name} {user.last_name} прошёл тест.\n\nТест: {question}\n\nОтвет пользователя: {ans}\n\nОтвет правильный? {ans.correct}")
+        bot.sendMessage(chat_id=386589423,
+                        text=f"{user.first_name} {user.last_name} прошёл тест.\n\nТест: {question}\n\nОтвет пользователя: {ans}\n\nОтвет правильный? {ans.correct}")
     except:
         # в случае если уже была создана связь
         pass
@@ -94,4 +110,3 @@ def get_csv(request):
             [result.user.first_name, result.user.last_name, group_name, result.question, result.answer, result_correct])
 
     return response
-
