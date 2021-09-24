8de843ac083187f53cba0c2bdeb254c2a6986062
Michael <doctorbf3@gmail.com>
2021-09-24 10:39:13 +0500

    обнова2

--- employee_integration/app/urls.py
+++ employee_integration/app/urls.py
@@ -5,7 +5,6 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("csv/", views.get_csv, name="get_csv"),
-    path("send_emails_to_complete_quiz/", views.send_emails_to_complete_quiz, name="send_emails_to_complete_quiz"),
     path("quiz/<int:pk_quiz>/", views.quiz_view, name="quiz"),
     path("quiz/<int:pk_quiz>/pass/<int:pk_ans>", views.quiz_pass, name="quiz_pass"),
 ]
